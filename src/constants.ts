9bcda20a727705f24337a0542453f887a2a3a3be
Vipin Yadav <vipiny35@gmail.com>
2023-11-15 08:24:05 +0530

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const PORTAL_URL = process.env.PORTAL_URL || 'https://portal.jeeth.co.in';
-export const API_URL = process.env.API_URL || 'http://localhost:3000';
+export const PORTAL_URL = process.env.NEXT_PUBLIC_PORTAL_URL || 'https://portal.jeeth.co.in';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.jeeth.co.in';
